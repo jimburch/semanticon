8ba6d913b15830b7f2a30f9c561b869b3661abca
Jim Burch <jrburch1@gmail.com>
2025-12-28 20:24:26 -0700

    fix keyboard wrapping

--- src/components/EmojiKeyboard/EmojiKeyboard.tsx
+++ src/components/EmojiKeyboard/EmojiKeyboard.tsx
@@ -10,7 +10,7 @@ const EmojiKeyboard = () => {
   };
 
   return (
-    <EmojiPicker.Root onEmojiSelect={handleEmojiSelect}>
+    <EmojiPicker.Root onEmojiSelect={handleEmojiSelect} columns={9}>
       <EmojiPicker.Search />
       <EmojiPicker.Viewport>
         <EmojiPicker.Loading>Loading…</EmojiPicker.Loading>
